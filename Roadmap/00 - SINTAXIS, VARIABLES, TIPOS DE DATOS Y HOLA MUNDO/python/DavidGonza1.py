c861a56961628cd298ef2a27a0114eaee433dad6
DavidGonza1 <davidgonzalezcanon2008@gmail.com>
2024-12-17 07:08:43 -0500

    DavidGonza1.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DavidGonza1.py
@@ -0,0 +1,27 @@
+# https://www.python.org
+
+"""
+Este
+Es
+Un
+Comentario
+de
+varias
+lineas
+"""
+#Creacion de una variable
+mi_variable = "mi variable"
+
+#Pyton no tiene constantes inmutables por defecto
+CONSTANTE = "Esta es una constante" # se usan mayusculas por convencion
+
+name = "David" #cadena de texto
+integer = 10 # Entero 
+float = 10.5 # #Flotante
+bool = True  # Booleano verdadero
+bool = False  # Booleano falso
+string = "esto es una cadena de texto" #cadena de texto
+
+#Para imprimir en la terminal
+print =("Hola, Python!")
+
